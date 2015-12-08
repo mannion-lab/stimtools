a2cd8d4508aa94b67327cfe44ce2400ddf2a7c02
damien <damien@localhost>
2015-12-08 12:53:30 +1100

    -

--- stim/utils/mask.py
+++ stim/utils/mask.py
@@ -5,10 +5,12 @@ import numpy as np
 import stim.utils
 
 
-def circ_mask(
+def polar_mask(
     size_pix,
     outer_extent_norm=1.0,
     inner_extent_norm=0.0,
+    sector_start_deg=0.0,
+    sector_end_deg=360.0,
     mask_min=-1.0,
     mask_max=+1.0
 ):
@@ -17,14 +19,21 @@ def circ_mask(
 
     (y, x) = np.mgrid[-half_size:+half_size, -half_size:+half_size]
 
-    (_, r) = stim.utils.cart_to_pol(x, y)
+    (theta, r) = stim.utils.cart_to_pol(x, y)
 
     r = r / half_size
 
-    mask = np.logical_and(
+    r_mask = np.logical_and(
         r >= inner_extent_norm,
         r < outer_extent_norm
-    ).astype("float")
+    )
+
+    theta_mask = np.logical_and(
+        theta >= sector_start_deg,
+        theta < sector_end_deg
+    )
+
+    mask = np.logical_and(r_mask, theta_mask).astype("float")
 
     mask = stim.utils.interval_convert(
         value=mask,
@@ -32,4 +41,4 @@ def circ_mask(
         new_interval=[mask_min, mask_max]
     )
 
-    return mask
+    return theta
