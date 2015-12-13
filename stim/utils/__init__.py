60005297433295dc2b375704210d39cf952415d8
Damien J. Mannion <damien@djmannion.net>
2015-12-13 14:29:50 +1100

    -

--- stim/utils/__init__.py
+++ stim/utils/__init__.py
@@ -1,7 +1,9 @@
-from __future__ import absolute_import, print_function, division
 
 from .pad_image import pad_image, nearest_pow2
-from .cart_to_pol import cart_to_pol
 from .mask import polar_mask
-from .interval_convert import interval_convert
+from .conversions import
+    interval_convert,
+    math_to_nav_polar,
+    cart_to_pol,
+    pol_to_cart
 from .circ_dist import circ_dist
