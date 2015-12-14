088a77085ba280ce44785299da66cf1e7ccf03e4
Damien Mannion <d.mannion@unsw.edu.au>
2015-12-14 13:36:44 +1100

    -

--- stim/utils/__init__.py
+++ stim/utils/__init__.py
@@ -1,9 +1,10 @@
 
 from .pad_image import pad_image, nearest_pow2
 from .mask import polar_mask
-from .conversions import
+from .conversions import (
     interval_convert,
     math_to_nav_polar,
     cart_to_pol,
     pol_to_cart
+)
 from .circ_dist import circ_dist
