89767f7c68a505af09e49cf0bc7c4b9b2a0a3f72
Damien Mannion <d.mannion@unsw.edu.au>
2016-07-28 17:14:05 +1000

    -

--- stim/stim/__init__.py
+++ stim/stim/__init__.py
@@ -3,3 +3,4 @@ from .polar_grating import polar_grating
 from .sfm_boundary import SFMBoundary
 from .glass_pattern import GlassPattern
 from .fixation import Fixation
+from .fusion_lock import FusionLock
