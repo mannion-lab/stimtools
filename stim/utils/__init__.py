5a49752dac21879f6d906fa4f92e4f8a7b800998
Damien Mannion <d.mannion@unsw.edu.au>
2017-04-02 13:32:05 +1000

    adding support for full ffmpeg path, and some style fixes

--- stim/utils/__init__.py
+++ stim/utils/__init__.py
@@ -13,3 +13,21 @@ from .circ_dist import circ_dist
 from .psi import Psi, logistic, weibull
 from .vids import img_seq_to_vid
 from .exr import write_exr
+
+__all__ = [
+    "pad_image",
+    "nearest_pow2",
+    "polar_mask",
+    "interval_convert",
+    "math_to_nav_polar",
+    "cart_to_pol",
+    "pol_to_cart",
+    "sph_to_cart",
+    "cart_to_sph",
+    "circ_dist",
+    "Psi",
+    "logistic",
+    "weibull",
+    "img_seq_to_vid",
+    "write_exr"
+]
