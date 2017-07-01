0c17dd64814a517211f1825949cfd67b39c21ab3
damien <damien@localhost>
2017-07-01 11:44:30 +1000

    -

--- stimtools/audio/noise.py
+++ stimtools/audio/noise.py
@@ -4,13 +4,107 @@ import collections
 import warnings
 
 import numpy as np
-
-import soundfile
+import scipy.io
 
 import stimtools.utils
 
 
-# TODO: update to use 'soundfile'
+def white_noise(
+    dur_s,
+    rms,
+    same_lr=True,
+    filename=None,
+    rate=44100,
+    window_samples=220,
+    post_pad_samples=0,
+    out_of_range=None,
+    rand=None
+):
+    """Generates a 'white noise' waveform (Gaussian noise).
+
+    Parameters
+    ----------
+    dur_s: float
+        Duration, in seconds.
+    rms: float or two-item sequences of floats
+        Root-mean-square amplitude for the L and R channels.
+    same_lr: bool, optional
+        Whether the left and right channels have the same random samples.
+    filename: string or None, optional
+        If provided, saves the waveform as a 'wav' file.
+    rate: int, optional
+        Sample rate.
+    window_samples: int, optional
+        Number of samples to use in a Hanning window at the start and end of
+        the waveform.
+    post_pad_samples: int, optional
+        The number of zeros to append to the waveform.
+    out_of_range: string, {"warn", "err"}, or None, optional
+        What to do if the waveform goes out of range.
+    rand: np.random.RandomState instance or None, optional
+        Random number generator.
+
+    Returns
+    -------
+    y : numpy array of 16-bit integers
+        A 2D array (number of samples x 2).
+
+    """
+
+    if rand is None:
+        rand = np.random.RandomState()
+
+    if not isinstance(rms, collections.Sequence):
+        rms = [rms] * 2
+
+    rms = np.array(rms)
+
+    n_samples = int(dur_s * rate)
+
+    if same_lr:
+        y = rand.normal(loc=0.0, scale=1.0, size=(n_samples, 1))
+        y = np.tile(y, (1, 2))
+    else:
+        y = rand.normal(loc=[0.0] * 2, scale=[1.0] * 2, size=(n_samples, 2))
+
+    if window_samples > 0:
+        y = stimtools.utils.apply_hanning(y, window_samples)
+
+    y *= rms
+
+    max_amp = 32767.0
+
+    y *= max_amp
+
+    if out_of_range in ("warn", "error"):
+
+        clip_req = np.logical_or(
+            np.any(y < -max_amp),
+            np.any(y > +max_amp)
+        )
+
+        if clip_req:
+
+            if out_of_range == "warn":
+                warnings.warn("Clipping required")
+            else:
+                raise ValueError("Clipping would be required")
+
+    y = np.clip(y, a_min=-max_amp, a_max=max_amp)
+
+    y = np.concatenate((y, np.zeros((post_pad_samples, 2))))
+
+    y = y.astype("int16")
+
+    if filename is not None:
+        scipy.io.wavfile.write(
+            filename=filename,
+            rate=rate,
+            data=y
+        )
+
+    return y
+
 
 def pink_noise(
     dur_s,
