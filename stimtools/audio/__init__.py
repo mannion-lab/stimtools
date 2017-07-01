d4652c0f52dad7c849078119280d7e1d1cdff7e7
Damien Mannion <d.mannion@unsw.edu.au>
2017-07-01 14:29:59 +1000

    -

--- stimtools/audio/__init__.py
+++ stimtools/audio/__init__.py
@@ -1,12 +1,13 @@
 
 from .tone import pure_tone
 from .noise import pink_noise, white_noise
-from .hardware import AudioFileParallel, AudioFileSerial
+from .hardware import AudioFileParallel, AudioFileSerial, write_playlist
 
 __all__ = [
     "pure_tone",
     "pink_noise",
     "white_noise",
     "AudioFileParallel",
-    "AudioFileSerial"
+    "AudioFileSerial",
+    "write_playlist"
 ]
