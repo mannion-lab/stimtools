2436549edcd146762f547039b5ee35fcd8d558d5
Damien Mannion <d.mannion@unsw.edu.au>
2018-03-04 15:03:27 +1100

    -

--- stimtools/visual/__init__.py
+++ stimtools/visual/__init__.py
@@ -1,13 +1,13 @@
 
-from .dpp import DisplayPlusPlus
-from .polar_grating import polar_grating
-from .sfm_boundary import SFMBoundary
-from .glass_pattern import GlassPattern
-from .fixation import Fixation
-from .fusion_lock import FusionLock
-from .arrow import Arrow
-from .rep_tex import rep_tex
-from .noise import noise_image
+from ._dpp import DisplayPlusPlus
+from ._polar_grating import polar_grating
+from ._sfm_boundary import SFMBoundary
+from ._glass_pattern import GlassPattern
+from ._fixation import Fixation
+from ._fusion_lock import FusionLock
+from ._arrow import Arrow
+from ._rep_tex import rep_tex
+from ._noise import noise_image
 
 __all__ = [
     "DisplayPlusPlus",
