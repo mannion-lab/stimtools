50522a52146997be034866b771c22dd84824c549
Damien Mannion <d.mannion@unsw.edu.au>
2018-03-17 13:29:34 +1100

    -

---
+++ stimtools/audio/_io.py
@@ -0,0 +1,13 @@
+
+import soundfile
+
+
+def save(wav_path, waveform, sr, wav_type="PCM_16"):
+
+    soundfile.write(
+        file=wav_path,
+        data=waveform,
+        samplerate=sr,
+        format="WAV",
+        subtype=wav_type
+    )
