d143f9d7d37ed6c1cb393f87ef842d3344e5ed07
Damien Mannion <d.mannion@unsw.edu.au>
2018-03-21 17:01:44 +1100

    -

--- stimtools/audio/hardware/_sc.py
+++ stimtools/audio/hardware/_sc.py
@@ -11,12 +11,36 @@ class SoundCard(object):
         **extra_settings
     ):
 
+        self._waveform = np.zeros(0)
+
         self._stream = sounddevice.OutputStream(
             callback=self._callback,
+            channels=2,
             **extra_settings
         )
 
         self._stream.start()
+
+    def _callback(self, outdata, frames, time, status):
+
+        if status:
+            print(status)
+
+        if len(self._waveform) == 0:
+           outdata.fill(0.0)
+
+        else:
+
+            if self._waveform.shape[0] < frames:
+                waveform = np.zeros(
+                    (frames, 2),
+                    dtype=self._waveform.dtype
+                )
+                waveform[:self._waveform.shape[0], :] = self._waveform
+                self._waveform = waveform
+
+            outdata[:] = self._waveform[:frames, :]
+            self._waveform = self._waveform[frames:, :]
 
     def __enter__(self):
         return self
@@ -24,26 +48,23 @@ class SoundCard(object):
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
 
-    def _callback(indata, outdata, frames, time, status):
-
-        if len(self._cued_waveform) == 0:
-            outdata.fill(0.0)
-
-        else:
-            outdata[:] = self._cued_waveform[:frames, :]
-            self._cued_waveform = self._cued_waveform[frames:, :]
-
     def cue(self, waveform):
+
+        if waveform.ndim != 2:
+            waveform = np.repeat(
+                waveform[:, np.newaxis],
+                (1, 2)
+            )
+
+        if waveform.dtype != np.float32:
+            waveform = waveform.astype("float32")
+
         self._cued_waveform = waveform
 
     def play(self):
 
-        if self._cued_waveform is None:
-            raise ValueError("Attempted to play but no waveform was cued")
+        self._waveform = self._cued_waveform
 
-        self._stream.write(self._cued_waveform)
-
-        self._cued_waveform = None
 
     def stop(self):
         self._stream.stop()
