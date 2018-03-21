bcd3c3bb2c925314ee80c83543e6e958b7ab770d
Damien Mannion <d.mannion@unsw.edu.au>
2018-03-21 14:05:34 +1100

    -

--- stimtools/audio/hardware/_sc.py
+++ stimtools/audio/hardware/_sc.py
@@ -1,15 +1,46 @@
 
+import numpy as np
+
+import sounddevice
+
 
 class SoundCard(object):
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        **extra_settings
+    ):
+
+        self._stream = sounddevice.OutputStream(
+            callback=self._callback,
+            **extra_settings
+        )
+
+        self._stream.start()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def _callback(indata, outdata, frames, time, status):
+        outdata[:] = (np.random.uniform(-1,1,outdata.shape).astype("float32"))
+
+    def cue(self, waveform):
+        self._cued_waveform = waveform
 
     def play(self):
-        pass
+
+        if self._cued_waveform is None:
+            raise ValueError("Attempted to play but no waveform was cued")
+
+        self._stream.write(self._cued_waveform)
+
+        self._cued_waveform = None
 
     def stop(self):
-        pass
+        self._stream.stop()
 
     def close(self):
-        pass
+        self._stream.close()
