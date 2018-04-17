8d0cc508e867c512de873606735075906e8af21e
Damien Mannion <d.mannion@unsw.edu.au>
2018-04-17 17:01:13 +1000

    -

--- stimtools/utils/_scramble.py
+++ stimtools/utils/_scramble.py
@@ -2,6 +2,64 @@
 import numpy as np
 
 
+def new_scramble_image(img, scramble="phase", i_end=0, seed=None):
+    """Perform scrambling on an image in freqency space.
+
+    Parameters
+    ----------
+    img: 2D or 3D numpy array of floats
+        Image to scramble
+    scramble: string, ["phase", "amplitude"], optional
+        What dimension to scramble
+    only_scramble_single_channel: bool
+        If ``img`` is 3D, this determines whether to scramble the third
+        dimension with one set of random values or multiple. With
+        single-channel scrambling, which Yoonessi & Kingdom (2008) call
+        'phase-aligned phase-scrambling', the colour histogram is better
+        preserved.
+
+    """
+
+    noise_shape = np.array(img.shape)[axes, ]
+
+    tile_k = np.array(img.shape)
+    tile_k[axes, ] = 1
+
+    rand = np.random.RandomState(seed=seed)
+
+    # convert the image to frequency space
+    img_freq = np.fft.fftn(a=img)
+
+    # get a frequency space representation of random noise
+    noise_freq = np.fft.fftn(a=rand.rand(*noise_shape))
+
+
+
+    out_amp = np.abs(img_freq)
+    out_phase = np.angle(img_freq)
+
+    if scramble == "phase":
+        out_phase = np.tile(np.angle(noise_freq), tile_k)
+
+    elif scramble == "amplitude":
+        out_amp[noise_slice] = np.abs(noise_freq)
+
+    print out_amp.shape, out_phase.shape
+
+    # combine the image's amplitude spectrum with the random phase
+    img_scrambled_freq = (
+        out_amp * np.cos(out_phase) +
+        1j * (out_amp * np.sin(out_phase))
+    )
+
+    # convert back into image space
+    img_scrambled = np.real(
+        np.fft.ifftn(a=img_scrambled_freq)
+    )
+
+    return img_scrambled
+
+
 def scramble_image(img, scramble="phase", only_scramble_single_channel=False):
     """Perform scrambling on an image in freqency space.
 
