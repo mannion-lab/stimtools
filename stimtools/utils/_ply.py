a31de14d0c5e5127f9e78b94c731f6ff8f9efc6a
Damien Mannion <d.mannion@unsw.edu.au>
2018-06-16 13:11:22 +1000

    -

--- stimtools/utils/_ply.py
+++ stimtools/utils/_ply.py
@@ -7,7 +7,7 @@ except ImportError:
     pass
 
 
-def create_ply(vertices, faces, save_path=None):
+def create_ply(vertices, faces, save_path=None, text=True):
 
     vertex_dtype = [(dim, "f4") for dim in "xyz"]
     vertex_array = np.array(vertices, dtype=vertex_dtype)
@@ -17,7 +17,7 @@ def create_ply(vertices, faces, save_path=None):
     face_array = np.array(faces, dtype=face_dtype)
     face_el = plyfile.PlyElement.describe(face_array, "face")
 
-    data = plyfile.PlyData([vertex_el, face_el], text=True)
+    data = plyfile.PlyData([vertex_el, face_el], text=text)
 
     if save_path is not None:
         data.write(save_path)
