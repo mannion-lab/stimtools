fd0c885356d7d8f65ea3d292fc7305f95f9d7009
Damien Mannion <d.mannion@unsw.edu.au>
2019-01-30 16:13:06 +1100

    -

--- stimtools/audio/_stats.py
+++ stimtools/audio/_stats.py
@@ -6,11 +6,14 @@ def rms_over_time(waveform, bin_samples):
 
     half_bin = int(bin_samples / 2)
 
-    rms = np.array(
-        [
-            np.std(waveform[(i_curr - half_bin):(i_curr + half_bin)])
-            for i_curr in range(half_bin, len(waveform) - half_bin)
-        ]
-    )
+    i_slice = np.arange(bin_samples) - half_bin
+
+    (n_samples, _) = waveform.shape
+
+    i_bins = i_slice[np.newaxis, :] + np.arange(n_samples)[:, np.newaxis]
+
+    i_bins = i_bins[half_bin:-(half_bin - 1), :]
+
+    rms = np.sqrt(np.mean(waveform[i_bins, :] ** 2, axis=(1, 2)))
 
     return rms
