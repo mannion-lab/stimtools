6d964ec6526382df02beba3a5c9760e159187bd5
Damien Mannion <d.mannion@unsw.edu.au>
2019-10-01 16:39:17 +1000

    -mesh2

--- stimtools/utils/_mesh.py
+++ stimtools/utils/_mesh.py
@@ -65,3 +65,70 @@ def pymesh_to_povray(mesh, smooth=True, save_path=None):
             save_file.write(pov)
 
     return pov
+
+
+def pymesh_to_povray_mesh2(mesh, smooth=True, save_path=None):
+    """Converts a `pymesh` mesh to POVray 'mesh2' format.
+
+    Parameters
+    ----------
+    mesh: `pymesh.Mesh` object
+        Mesh to export.
+    smooth: bool, optional
+        Whether to interpolate over the faces.
+    save_path: str-like, optional
+        If provided, save the POVray code to this file.
+
+    Returns
+    -------
+    pov: str
+        POVray `mesh` definition.
+
+    """
+
+    pov = "mesh2 {\n"
+
+    (n_vertices, _) = mesh.vertices.shape
+
+    pov += f"vertex_vectors {{ {n_vertices:d}"
+
+    for vertex in mesh.vertices:
+        pov += ","
+        pov += "<" + ",".join([f"{dim_vertex:.12f}" for dim_vertex in vertex]) + ">"
+
+    pov += "\n}\n"
+
+    (n_faces, _) = mesh.faces.shape
+
+    pov += f"face_indices {{ {n_faces:d}"
+
+    for face in mesh.faces:
+        pov += ","
+        pov += "<" + ",".join([f"{dim_i:d}" for dim_i in face]) + ">"
+
+    pov += "\n}\n"
+
+    if smooth:
+
+        if not mesh.has_attribute("vertex_normal"):
+            mesh.add_attribute("vertex_normal")
+
+        normals = mesh.get_attribute("vertex_normal")
+
+        normals = normals.reshape((mesh.num_vertices, 3))
+
+        pov += f"normal_vectors {{ {n_vertices:d}"
+
+        for normal in normals:
+            pov += ","
+            pov += "<" + ",".join([f"{dim_normal:.12f}" for dim_normal in normal]) + ">"
+
+        pov += "\n}\n"
+
+    pov += "}"
+
+    if save_path is not None:
+        with open(save_path, "w") as save_file:
+            save_file.write(pov)
+
+    return pov
