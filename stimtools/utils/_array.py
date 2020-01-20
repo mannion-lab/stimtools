1e3df5d8f87929306e1f3f297d3d380678fbf12d
Damien Mannion <d.mannion@unsw.edu.au>
2020-01-20 11:29:13 +1100

    -

--- stimtools/utils/_array.py
+++ stimtools/utils/_array.py
@@ -4,6 +4,8 @@ import numpy as np
 def add_empty_axes(arr, n_pre=0, n_post=0):
     """Add empty axes before and after the structure of an array."""
 
+    arr = np.array(arr)
+
     for _ in range(n_pre):
         arr = arr[np.newaxis, ...]
 
