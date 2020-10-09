38aa3668084ae1216d5bc58f9ed2c11a39faca60
Damien Mannion <d.mannion@unsw.edu.au>
2020-10-09 15:19:08 +1100

    -

--- stimtools/utils/_cubemap.py
+++ stimtools/utils/_cubemap.py
@@ -11,11 +11,13 @@ except ImportError:
     pass
 
 
-def panorama_to_cubemap(panorama_img, face_size=1024):
+def panorama_to_cubemap(panorama_img, face_size=1024, cube_format="dice"):
 
     if not isinstance(panorama_img, np.ndarray):
         panorama_img = imageio.imread(panorama_img)
 
-    cubemap_img = py360convert.e2c(e_img=panorama_img, face_w=face_size)
+    cubemap_img = py360convert.e2c(
+        e_img=panorama_img, face_w=face_size, cube_format=cube_format
+    )
 
     return cubemap_img
